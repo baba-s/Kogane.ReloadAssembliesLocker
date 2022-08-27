24abb402f7a4e042f0be04449b67d9243e9c2b6d
baba-s <shotababa1206@gmail.com>
2022-08-27 15:00:45 +0900

    update

--- Editor/ReloadAssembliesLocker.cs
+++ Editor/ReloadAssembliesLocker.cs
@@ -2,46 +2,46 @@
 
 namespace Kogane.Internal
 {
-	[InitializeOnLoad]
-	internal static class ReloadAssembliesLocker
-	{
-		private static bool m_isLock;
+    [InitializeOnLoad]
+    internal static class ReloadAssembliesLocker
+    {
+        private static bool m_isLock;
 
-		static ReloadAssembliesLocker()
-		{
-			EditorApplication.delayCall += () => UpdateChecked();
+        static ReloadAssembliesLocker()
+        {
+            EditorApplication.delayCall += () => UpdateChecked();
 
-			void OnChanged( PlayModeStateChange change )
-			{
-				if ( change != PlayModeStateChange.ExitingEditMode ) return;
-				Unlock();
-			}
+            void OnChanged( PlayModeStateChange change )
+            {
+                if ( change != PlayModeStateChange.ExitingEditMode ) return;
+                Unlock();
+            }
 
-			EditorApplication.playModeStateChanged += OnChanged;
-		}
+            EditorApplication.playModeStateChanged += OnChanged;
+        }
 
-		[MenuItem( "Edit/UniReloadAssembliesLocker/Lock" )]
-		private static void Lock()
-		{
-			if ( m_isLock ) return;
-			m_isLock = true;
-			EditorApplication.LockReloadAssemblies();
-			UpdateChecked();
-		}
+        [MenuItem( "Kogane/Reload Assemblies/Lock" )]
+        private static void Lock()
+        {
+            if ( m_isLock ) return;
+            m_isLock = true;
+            EditorApplication.LockReloadAssemblies();
+            UpdateChecked();
+        }
 
-		[MenuItem( "Edit/UniReloadAssembliesLocker/Unlock" )]
-		private static void Unlock()
-		{
-			if ( !m_isLock ) return;
-			m_isLock = false;
-			EditorApplication.UnlockReloadAssemblies();
-			UpdateChecked();
-		}
+        [MenuItem( "Kogane/Reload Assemblies/Unlock" )]
+        private static void Unlock()
+        {
+            if ( !m_isLock ) return;
+            m_isLock = false;
+            EditorApplication.UnlockReloadAssemblies();
+            UpdateChecked();
+        }
 
-		private static void UpdateChecked()
-		{
-			Menu.SetChecked( "Edit/UniReloadAssembliesLocker/Lock", m_isLock );
-			Menu.SetChecked( "Edit/UniReloadAssembliesLocker/Unlock", !m_isLock );
-		}
-	}
+        private static void UpdateChecked()
+        {
+            Menu.SetChecked( "Kogane/Reload Assemblies/Lock", m_isLock );
+            Menu.SetChecked( "Kogane/Reload Assemblies/Unlock", !m_isLock );
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UniReloadAssembliesLocker
+# Kogane Reload Assemblies Locker
 
 コンパイルを無効化できるエディタ拡張
 
@@ -6,14 +6,14 @@
 
 ![image (25)](https://user-images.githubusercontent.com/6134875/87114355-51746480-c2ab-11ea-8d93-e9d2cfe885dc.gif)
 
-通常、Unity 上でスクリプトを追加すると、スクリプトが追加されるたびにコンパイルが走りますが、  
+通常、Unity 上でスクリプトを追加すると、スクリプトが追加されるたびにコンパイルが走りますが、
 
 ![2020-07-10_123943](https://user-images.githubusercontent.com/6134875/87114356-533e2800-c2ab-11ea-8fc2-1d15e8014be6.png)
 
-Unity メニューの「Edit > UniReloadAssembliesLocker > Lock」をオンにすることで  
+Unity メニューの「Edit > UniReloadAssembliesLocker > Lock」をオンにすることで
 
 ![image (26)](https://user-images.githubusercontent.com/6134875/87114360-55a08200-c2ab-11ea-9ba9-d7b38b10d55c.gif)
 
-スクリプトを追加してもコンパイルが走らないようになります  
+スクリプトを追加してもコンパイルが走らないようになります
 
 Unity を再生する、もしくは「Edit > UniReloadAssembliesLocker > Unlock」をオンにするとコンパイルが走ります  
